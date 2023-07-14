eb4b6f35681f2eb9025aafe4c3b415eddde0ba51
MTaslam2000 <taiyab.aslam728@gmail.com>
2023-07-14 11:14:55 -0500

    idk

---
+++ main.js
@@ -0,0 +1,6 @@
+// console.log("Hello, world!");
+let moment = require("moment");
+let day = new Date(2011, 9, 16);
+let dayWrapper = moment(day);
+
+console.log("The current date is: " + moment().format("YYYY"));
